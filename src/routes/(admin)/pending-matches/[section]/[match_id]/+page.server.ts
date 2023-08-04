ac97f5178a87d4c3db73f111e6dde388cd05c7ec
Gihyun <giordnnuz@gmail.com>
2023-08-04 10:43:09 +0800

    Debugging (12)

--- src/routes/(admin)/pending-matches/[section]/[match_id]/+page.server.ts
+++ src/routes/(admin)/pending-matches/[section]/[match_id]/+page.server.ts
@@ -11,18 +11,18 @@ export const load: PageServerLoad = async ({ params, setHeaders }) => {
 
 	console.log(matchSetId);
 
-	// const matchSetRef = doc(db, `match_sets/${matchSetId}`);
-	// const matchSetDoc = await getDoc(matchSetRef);
+	const matchSetRef = doc(db, `match_sets/${matchSetId}`);
+	const matchSetDoc = await getDoc(matchSetRef);
 
-	// if (!matchSetDoc.exists) {
-	// 	throw new Error("Match set doesn't exist");
-	// }
+	if (!matchSetDoc.exists) {
+		throw new Error("Match set doesn't exist");
+	}
 
-	// const matchSet = matchSetDoc.data() as MatchSet;
-	// const matchesCollection = collection(db, `match_sets/${matchSetId}/matches`);
-	// const matchesDocs = await getDocs(matchesCollection);
+	const matchSet = matchSetDoc.data() as MatchSet;
+	const matchesCollection = collection(db, `match_sets/${matchSetId}/matches`);
+	const matchesDocs = await getDocs(matchesCollection);
 
-	// let matches = matchesDocs.docs.map((match) => JSON.parse(JSON.stringify(match.data())) as Match);
+	let matches = matchesDocs.docs.map((match) => JSON.parse(JSON.stringify(match.data())) as Match);
 
 	// const cardBattleCollection = collection(db, `match_sets/${matchSetId}/card_battle`);
 	// const cardBattleDocs = await getDocs(cardBattleCollection);
@@ -40,83 +40,83 @@ export const load: PageServerLoad = async ({ params, setHeaders }) => {
 	// 	matches = [];
 	// }
 
-	// setHeaders({ 'cache-control': `max-age=${CACHE_DURATION}, must-revalidate` });
+	setHeaders({ 'cache-control': `max-age=${CACHE_DURATION}, must-revalidate` });
 
 	return {
-		matchSetId
-		// matches,
+		matchSetId,
+		matches
 		// matchSet,
 		// cardBattle
 	};
 };
 
-export const actions: Actions = {
-	card_battle: async ({ params, request }) => {
-		const matchSetId = params.match_id;
+// export const actions: Actions = {
+// 	card_battle: async ({ params, request }) => {
+// 		const matchSetId = params.match_id;
 
-		if (!matchSetId) {
-			throw error(404, 'Match set not found.');
-		}
+// 		if (!matchSetId) {
+// 			throw error(404, 'Match set not found.');
+// 		}
 
-		const data = await request.formData();
-		const cardBattle = data.get('card_battle')?.toString();
+// 		const data = await request.formData();
+// 		const cardBattle = data.get('card_battle')?.toString();
 
-		if (!cardBattle) {
-			throw new Error('Cards are undefined');
-		}
+// 		if (!cardBattle) {
+// 			throw new Error('Cards are undefined');
+// 		}
 
-		const parsedCardBattle = JSON.parse(cardBattle) as CardBattle[];
+// 		const parsedCardBattle = JSON.parse(cardBattle) as CardBattle[];
 
-		const cardBattleResults = await runCardBattle(parsedCardBattle, matchSetId);
+// 		const cardBattleResults = await runCardBattle(parsedCardBattle, matchSetId);
 
-		return { cardBattleResults };
-	}
-};
+// 		return { cardBattleResults };
+// 	}
+// };
 
-async function updateCardBattleDocument(cardBattle: CardBattle[], matchSetId: string, idx: number) {
-	const match = cardBattle[idx];
-	const player1 = match.players[0];
-	const player2 = match.players[1];
+// async function updateCardBattleDocument(cardBattle: CardBattle[], matchSetId: string, idx: number) {
+// 	const match = cardBattle[idx];
+// 	const player1 = match.players[0];
+// 	const player2 = match.players[1];
 
-	const result = await battle(player1.auth_data.uid, player2.auth_data.uid);
+// 	const result = await battle(player1.auth_data.uid, player2.auth_data.uid);
 
-	console.log('Result in updateCardBattle');
-	console.log(result);
+// 	console.log('Result in updateCardBattle');
+// 	console.log(result);
 
-	const player1TotalDamage = result[0].totalDamage;
-	const player2TotalDamage = result[1].totalDamage;
+// 	const player1TotalDamage = result[0].totalDamage;
+// 	const player2TotalDamage = result[1].totalDamage;
 
-	const cardBattleRef = doc(db, `match_sets/${matchSetId}/card_battle/${idx + 1}`);
+// 	const cardBattleRef = doc(db, `match_sets/${matchSetId}/card_battle/${idx + 1}`);
 
-	if (player1TotalDamage !== null) {
-		player1.total_damage = player1TotalDamage;
-	} else {
-		player1.total_damage = null;
-	}
+// 	if (player1TotalDamage !== null) {
+// 		player1.total_damage = player1TotalDamage;
+// 	} else {
+// 		player1.total_damage = null;
+// 	}
 
-	if (player2TotalDamage !== null) {
-		player2.total_damage = player2TotalDamage;
-	} else {
-		player2.total_damage = null;
-	}
+// 	if (player2TotalDamage !== null) {
+// 		player2.total_damage = player2TotalDamage;
+// 	} else {
+// 		player2.total_damage = null;
+// 	}
 
-	console.log('Updating doc');
-	await updateDoc(cardBattleRef, { players: [player1, player2] });
-}
+// 	console.log('Updating doc');
+// 	await updateDoc(cardBattleRef, { players: [player1, player2] });
+// }
 
-async function runCardBattle(cardBattle: CardBattle[], matchSetId: string): Promise<CardBattle[]> {
-	for (let idx = 0; idx < cardBattle.length; idx++) {
-		await updateCardBattleDocument(cardBattle, matchSetId, idx);
+// async function runCardBattle(cardBattle: CardBattle[], matchSetId: string): Promise<CardBattle[]> {
+// 	for (let idx = 0; idx < cardBattle.length; idx++) {
+// 		await updateCardBattleDocument(cardBattle, matchSetId, idx);
 
-		console.log('Refreshing cardBattle data...');
-	}
-	const cardBattleCollection = collection(db, `match_sets/${matchSetId}/card_battle`);
-	const snapshot = await getDocs(cardBattleCollection);
+// 		console.log('Refreshing cardBattle data...');
+// 	}
+// 	const cardBattleCollection = collection(db, `match_sets/${matchSetId}/card_battle`);
+// 	const snapshot = await getDocs(cardBattleCollection);
 
-	if (snapshot.empty) {
-		throw new Error('Card battle collection is empty.');
-	}
-	cardBattle = snapshot.docs.map((doc) => doc.data() as CardBattle);
+// 	if (snapshot.empty) {
+// 		throw new Error('Card battle collection is empty.');
+// 	}
+// 	cardBattle = snapshot.docs.map((doc) => doc.data() as CardBattle);
 
-	return cardBattle;
-}
+// 	return cardBattle;
+// }
--- src/routes/(admin)/pending-matches/[section]/[match_id]/+page.svelte
+++ src/routes/(admin)/pending-matches/[section]/[match_id]/+page.svelte
@@ -1,183 +1,197 @@
 <script lang="ts">
-	// import { enhance } from '$app/forms';
-	// import { CircleCheckFilled, ClockPause } from '$lib/assets/icons/index.js';
-	// import { db } from '$lib/firebase/firebase';
-	// import type { UserData } from '$lib/types';
-	// import { Timestamp, addDoc, collection } from 'firebase/firestore';
+	import { enhance } from '$app/forms';
+	import { CircleCheckFilled, ClockPause } from '$lib/assets/icons/index.js';
+	import { db } from '$lib/firebase/firebase';
+	import type { Match, UserData } from '$lib/types';
+	import { Timestamp, addDoc, collection, onSnapshot } from 'firebase/firestore';
+	import { onDestroy } from 'svelte';
 
 	export let data;
 
-	// $: ({ matchSetId } = data);
+	$: ({ matchSetId, matches } = data);
 
-	// let clickedRow: number | null = null;
+	let clickedRow: number | null = null;
 
-	// function toggleRow(idx: number) {
-	// 	clickedRow = clickedRow === idx ? null : idx;
-	// }
+	function toggleRow(idx: number) {
+		clickedRow = clickedRow === idx ? null : idx;
+	}
 
-	// function addToMatchHistory(users: UserData[]) {
-	// 	const currentDate = new Date();
+	function addToMatchHistory(users: UserData[]) {
+		const currentDate = new Date();
 
-	// 	const matchHistoryData = {
-	// 		players: [...users],
-	// 		timestamp: Timestamp.fromDate(currentDate)
-	// 	};
+		const matchHistoryData = {
+			players: [...users],
+			timestamp: Timestamp.fromDate(currentDate)
+		};
 
-	// 	users.forEach(async (user) => {
-	// 		const matchHistoryCollection = collection(db, `users/${user.auth_data.uid}/match_history`);
+		users.forEach(async (user) => {
+			const matchHistoryCollection = collection(db, `users/${user.auth_data.uid}/match_history`);
 
-	// 		await addDoc(matchHistoryCollection, matchHistoryData);
-	// 	});
-	// }
+			await addDoc(matchHistoryCollection, matchHistoryData);
+		});
+	}
+
+	$: {
+		if (matchSetId) {
+			const matchSetCollection = collection(db, `match_sets/${matchSetId}/matches`);
+			const unsubMatches = onSnapshot(matchSetCollection, (snapshot) => {
+				matches = snapshot.docs.map((match) => match.data() as Match);
+			});
+
+			console.log('Match set snapshot ran: ' + matchSetId);
+
+			onDestroy(() => unsubMatches());
+		}
+	}
 
 	// // Might change this to a form action instead?
-	// async function handleSubmit(event: SubmitEvent, users: UserData[], matchSetId: string) {
-	// 	const form = event?.target as HTMLFormElement;
-	// 	const formData = new FormData(form);
+	async function handleSubmit(event: SubmitEvent, users: UserData[], matchSetId: string) {
+		const form = event?.target as HTMLFormElement;
+		const formData = new FormData(form);
 
-	// 	formData.append('matchSetId', matchSetId);
-	// 	formData.append('userUid', users[0].auth_data.uid);
+		formData.append('matchSetId', matchSetId);
+		formData.append('userUid', users[0].auth_data.uid);
 
-	// 	const response = await fetch('/api/submit-score', {
-	// 		method: 'POST',
-	// 		body: formData
-	// 	});
+		const response = await fetch('/api/submit-score', {
+			method: 'POST',
+			body: formData
+		});
 
-	// 	if (!response.ok) {
-	// 		throw new Error('Error in submitting score: ' + response.statusText);
-	// 	}
+		if (!response.ok) {
+			throw new Error('Error in submitting score: ' + response.statusText);
+		}
 
-	// 	console.log('Scores submitted successfully!');
-	// 	form.reset();
-	// 	addToMatchHistory(users);
-	// }
+		console.log('Scores submitted successfully!');
+		form.reset();
+		addToMatchHistory(users);
+	}
 
 	// type BattleTab = 'arnis' | 'card_battle';
 
 	// let currentTab: BattleTab = 'arnis';
 </script>
 
-<div>{data.matchSetId}</div>
+<div>{matchSetId}</div>
 <!-- <div class="flex gap-4">
 	<button class="btn variant-filled" on:click={() => (currentTab = 'arnis')}>Arnis</button>
 	<button class="btn variant-filled" on:click={() => (currentTab = 'card_battle')}>Cards</button>
 </div> -->
 
 <div>hello</div>
-<!-- 
-{#if currentTab === 'arnis'}
-	<div class="table-container max-w-5xl">
-		<table class="table-hover table-interactive table-compact table">
-			<thead>
-				<tr class="text-sm md:text-base">
-					<th>Player 1</th>
-					<th>VS</th>
-					<th>Player 2</th>
-					<th>Skill</th>
-					<th>Footwork</th>
-					<th>Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each matches as match, idx (idx)}
-					{@const players = match.players}
 
-					<tr
-						class={`${
-							match.status === 'finished' ? 'pointer-events-none opacity-50' : 'opacity-100'
-						}`}
-						on:click={() => toggleRow(idx)}
-					>
-						{#each players as player, idx (player.auth_data.uid)}
-							{@const name = `${player.personal_data.name.first} ${player.personal_data.name.last}`}
-							<td class="flex gap-2">
-								<span class="text-xs md:text-sm">{name}</span>
-								{#if player.power_cards.find((card) => card.activated && !card.used)}
-									<div>
-										{#each player.power_cards as card, idx (idx)}
-											{#if card.activated && !card.used}
-												{@const words = card.name.split(' ')}
-												<span class="uppercase">
-													{#each words as word, idx (idx)}
-														{word[0]}
-													{/each}
-												</span>
-											{/if}
-										{/each}
-									</div>
-								{/if}
+<!-- {#if currentTab === 'arnis'} -->
+<div class="table-container max-w-5xl">
+	<table class="table-hover table-interactive table-compact table">
+		<thead>
+			<tr class="text-sm md:text-base">
+				<th>Player 1</th>
+				<th>VS</th>
+				<th>Player 2</th>
+				<th>Skill</th>
+				<th>Footwork</th>
+				<th>Status</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each matches as match, idx (idx)}
+				{@const players = match.players}
+
+				<tr
+					class={`${
+						match.status === 'finished' ? 'pointer-events-none opacity-50' : 'opacity-100'
+					}`}
+					on:click={() => toggleRow(idx)}
+				>
+					{#each players as player, idx (player.auth_data.uid)}
+						{@const name = `${player.personal_data.name.first} ${player.personal_data.name.last}`}
+						<td class="flex gap-2">
+							<span class="text-xs md:text-sm">{name}</span>
+							{#if player.power_cards.find((card) => card.activated && !card.used)}
+								<div>
+									{#each player.power_cards as card, idx (idx)}
+										{#if card.activated && !card.used}
+											{@const words = card.name.split(' ')}
+											<span class="uppercase">
+												{#each words as word, idx (idx)}
+													{word[0]}
+												{/each}
+											</span>
+										{/if}
+									{/each}
+								</div>
+							{/if}
+						</td>
+						{#if idx < 1}
+							<td>
+								<span class="uppercase text-primary-500-400-token">vs</span>
 							</td>
-							{#if idx < 1}
-								<td>
-									<span class="uppercase text-primary-500-400-token">vs</span>
-								</td>
-							{/if}
-						{/each}
-						<td class="w-44">
-							<p class="text-xs md:text-sm">{match.skill}</p>
-						</td>
-						<td class="w-44">
-							<p class="text-xs md:text-sm">{match.footwork}</p>
-						</td>
-						<td class="w-20">
-							{#if match.status === 'pending'}
-								<div title="Pending">
-									<ClockPause styles="w-5 h-5 text-yellow-500" />
-								</div>
-							{:else}
-								<div title="Finished">
-									<CircleCheckFilled styles="w-5 h-5 text-green-500" />
-								</div>
-							{/if}
-						</td>
-					</tr>
+						{/if}
+					{/each}
+					<td class="w-44">
+						<p class="text-xs md:text-sm">{match.skill}</p>
+					</td>
+					<td class="w-44">
+						<p class="text-xs md:text-sm">{match.footwork}</p>
+					</td>
+					<td class="w-20">
+						{#if match.status === 'pending'}
+							<div title="Pending">
+								<ClockPause styles="w-5 h-5 text-yellow-500" />
+							</div>
+						{:else}
+							<div title="Finished">
+								<CircleCheckFilled styles="w-5 h-5 text-green-500" />
+							</div>
+						{/if}
+					</td>
+				</tr>
 
-					Separate component? -->
-<!-- Change to dialog -->
-<!-- Modal
-					{#if clickedRow === idx}
-						<div class="fixed left-0 top-0 z-[999] h-full w-full bg-surface-backdrop-token">
-							<div class="flex h-full w-full items-center justify-center">
-								<div
-									class="w-modal space-y-4 p-4 shadow-xl bg-surface-100-800-token rounded-container-token"
+				<!-- Separate component? -->
+				<!-- Change to dialog -->
+				<!-- Modal -->
+				{#if clickedRow === idx}
+					<div class="fixed left-0 top-0 z-[999] h-full w-full bg-surface-backdrop-token">
+						<div class="flex h-full w-full items-center justify-center">
+							<div
+								class="w-modal space-y-4 p-4 shadow-xl bg-surface-100-800-token rounded-container-token"
+							>
+								<form
+									class="space-y-4"
+									on:submit|preventDefault={(e) => handleSubmit(e, match.players, matchSetId)}
 								>
-									<form
-										class="space-y-4"
-										on:submit|preventDefault={(e) => handleSubmit(e, match.players, matchSetId)}
-									>
-										{#each match.players as user (user.auth_data.uid)}
-											<label class="label">
-												<span>
-													Score for
-													{user.personal_data.name.first}
-													{user.personal_data.name.last}
-												</span>
-												<input
-													class="input text-token"
-													type="text"
-													name={`score-${user.auth_data.uid}`}
-													required
-												/>
-											</label>
-										{/each}
-										<div class="flex justify-end gap-4">
-											<button
-												class="btn variant-ghost-surface text-token"
-												type="button"
-												on:click={() => (clickedRow = null)}>Cancel</button
-											>
-											<button class="btn variant-filled-primary" type="submit">Submit</button>
-										</div>
-									</form>
-								</div>
+									{#each match.players as user (user.auth_data.uid)}
+										<label class="label">
+											<span>
+												Score for
+												{user.personal_data.name.first}
+												{user.personal_data.name.last}
+											</span>
+											<input
+												class="input text-token"
+												type="text"
+												name={`score-${user.auth_data.uid}`}
+												required
+											/>
+										</label>
+									{/each}
+									<div class="flex justify-end gap-4">
+										<button
+											class="btn variant-ghost-surface text-token"
+											type="button"
+											on:click={() => (clickedRow = null)}>Cancel</button
+										>
+										<button class="btn variant-filled-primary" type="submit">Submit</button>
+									</div>
+								</form>
 							</div>
 						</div>
-					{/if}
-				{/each}
-			</tbody>
-		</table>
-	</div>
-{:else}
+					</div>
+				{/if}
+			{/each}
+		</tbody>
+	</table>
+</div>
+<!-- {:else}
 	<form
 		method="post"
 		action="?/card_battle"
